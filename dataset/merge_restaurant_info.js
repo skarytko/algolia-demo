b49383230fd480324caf6a9c8371109f5faace32
Stefan Karytko <stefan@karytko.com>
2026-08-25 11:17:49 -0400

    data set update

--- dataset/merge_restaurant_info.js
+++ dataset/merge_restaurant_info.js
@@ -8,20 +8,12 @@ const OUTPUT_PATH = path.join(__dirname, "restaurants_list_merged.json");
 const csvLines = fs.readFileSync(CSV_PATH, "utf8").trim().split("\n");
 const headers = csvLines[0].split(";");
 
-const NUMERIC_FIELDS = new Set(["stars_count", "reviews_count"]);
-
 const infoByObjectID = new Map();
 for (const line of csvLines.slice(1)) {
   const fields = line.split(";");
   const record = {};
   headers.forEach((header, i) => {
-    if (header === "stars_count") {
-      record[header] = Math.round(Number(fields[i]));
-    } else if (NUMERIC_FIELDS.has(header)) {
-      record[header] = Number(fields[i]);
-    } else {
-      record[header] = fields[i];
-    }
+    record[header] = fields[i];
   });
   infoByObjectID.set(record.objectID, record);
 }
